d6813a2cc0b7c376a748b2410219e3d462e968c7
juhee <dollypatel26791@gmail.com>
2021-12-22 17:15:39 -0800

    Fixing validation bug in Create entry page

--- resources/views/coffeeBeanTable/create.blade.php
+++ resources/views/coffeeBeanTable/create.blade.php
@@ -23,37 +23,37 @@
               <div class="col-xs-12 col-sm-12 col-md-12" >
                 <div class="form-group">
                     <strong>Name:</strong>
-                    <input type="text" name="Name" class="form-control" placeholder="name" >
+                    <input type="text" name="Name" class="form-control" placeholder="name" value={{old('Name')}} >
                 </div>
             </div>
             <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
                     <strong>Caffeine Level:</strong>
-                    <select id="Caffeine Level" name="Caffeine Level"  class="form-control">
-                    <option value="">--- Choose a Caffeine Level ---</option>
-                    <option value="decaf">decaf</option>
-                    <option value="low">low</option>
-                    <option value="medium">medium</option>
-                    <option value="high">high</option>
+                    <select name="Caffeine_Level"  class="form-control">
+                    <option value="" {{ old('Caffeine_Level') == "" ? 'selected' : '' }} >--- Choose a Caffeine Level ---</option>
+                    <option value="decaf" {{ old('Caffeine_Level') == "decaf" ? 'selected' : '' }} >decaf</option>
+                    <option value="low" {{ old('Caffeine_Level') == "low" ? 'selected' : '' }} >low</option>
+                    <option value="medium" {{ old('Caffeine_Level') == "medium" ? 'selected' : '' }} >medium</option>
+                    <option value="high" {{ old('Caffeine_Level') == "high" ? 'selected' : '' }} >high</option>
                     </select>
                  </div>
              </div>
              <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
                     <strong>Cost/lb:</strong>
-                    <input type="number" step="0.01" name="Cost_lb" class="form-control" placeholder="cost/lb">
+                    <input type="number" step="0.01" name="Cost_lb" class="form-control" placeholder="cost/lb" value={{old('Cost_lb')}}>
                 </div>
             </div>
             <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
                     <strong>Bean Type:</strong>
-                    <select id="Bean Type" name="Bean Type"  class="form-control">
-                    <option value="">--- Choose a Bean Type ---</option>
-                    <option value="Arabica">Arabica</option>
-                    <option value="Robusta">Robusta</option>
-                    <option value="Liberca">Liberca</option>
-                    <option value="Excelsa">Excelsa</option>
-                    <option value="Other">Other</option>
+                    <select name="Bean_Type"  class="form-control">
+                    <option value="" {{ old('Bean_Type') == "" ? 'selected' : '' }} >--- Choose a Bean Type ---</option>
+                    <option value="Arabica" {{ old('Bean_Type') == "Arabica" ? 'selected' : '' }} >Arabica</option>
+                    <option value="Robusta" {{ old('Bean_Type') == "Robusta" ? 'selected' : '' }} >Robusta</option>
+                    <option value="Liberca" {{ old('Bean_Type') == "Liberca" ? 'selected' : '' }} >Liberca</option>
+                    <option value="Excelsa" {{ old('Bean_Type') == "Excelsa" ? 'selected' : '' }} >Excelsa</option>
+                    <option value="Other" {{ old('Bean_Type') == "Other" ? 'selected' : '' }} >Other</option>
                     </select>
                  </div>
             </div>
@@ -61,30 +61,30 @@
                  <div class="form-group">
                      <strong>Roast:</strong>
                     <select id="Roast" name="Roast"  class="form-control">
-                    <option value="">--- Choose a Roast ---</option>
-                    <option value="dark">dark</option>
-                    <option value="medium">medium</option>
-                    <option value="light">light</option>
-                    <option value="french">french</option>
+                    <option value="" {{ old('Roast') == "" ? 'selected' : '' }} >--- Choose a Roast ---</option>
+                    <option value="dark" {{ old('Roast') == "dark" ? 'selected' : '' }} >dark</option>
+                    <option value="medium" {{ old('Roast') == "medium" ? 'selected' : '' }} >medium</option>
+                    <option value="light" {{ old('Roast') == "light" ? 'selected' : '' }} >light</option>
+                    <option value="french" {{ old('Roast') == "french" ? 'selected' : '' }} >french</option>
                     </select>
                  </div>
             </div>
             <div class="col-xs-12 col-sm-12 col-md-12">
                   <div class="form-group">
                       <strong>Grind:</strong>
-                    <select id="Grind" name="Grind"  class="form-control">
-                    <option value="">--- Choose a Grind ---</option>
-                    <option value=" whole-bean"> whole-bean</option>
-                    <option value="fine">fine</option>
-                    <option value="medium">medium</option>
-                    <option value="coarse">coarse</option>
+                    <select name="Grind"  class="form-control">
+                    <option value="" {{ old('Grind') == "" ? 'selected' : '' }} >--- Choose a Grind ---</option>
+                    <option value="whole-bean" {{ old('Grind') == "whole-bean" ? 'selected' : '' }} > whole-bean</option>
+                    <option value="fine" {{ old('Grind') == "fine" ? 'selected' : '' }} >fine</option>
+                    <option value="medium" {{ old('Grind') == "medium" ? 'selected' : '' }} >medium</option>
+                    <option value="coarse" {{ old('Grind') == "coarse" ? 'selected' : '' }} >coarse</option>
                     </select>
                 </div>
             </div>
             <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
                 <strong>Country of Origin:</strong>
-                <input type="text" name="Country_of_Origin" class="form-control" placeholder="country of origin">
+                <input type="text" name="Country_of_Origin" class="form-control" placeholder="country of origin" value={{old('Country_of_Origin')}}>
             </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12 text-center">
